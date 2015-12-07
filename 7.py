7a1d241817e2bb36c81db3fa19be47b4f0ce88e7
BoppreH <boppreh@gmail.com>
2015-12-07 04:27:18 -0200

    Slight improvements to 7

--- 7.py
+++ 7.py
@@ -1,35 +1,32 @@
 import re
 
+signals = {}
 def eval(variable):
-	value = raw_eval(variable)
-	values[variable] = value
-	return value
+	if variable in signals:
+		return signals[variable]
+	return signals.setdefault(variable, raw_eval(variable))
 
 def raw_eval(variable):
-	if variable in values:
-		return values[variable]
 	if variable.isdigit():
 		return int(variable)
 
-	instruction = instructions[variable]
+	parts = instructions[variable].split()
 
-	if 'NOT' in instruction:
-		_, value = instruction.split(' ')
-		return 2**16-1 - eval(value)
-	elif ' ' not in instruction:
-		return eval(instruction)
+	if len(parts) == 1:
+		return eval(parts[0])
+	elif len(parts) == 2:
+		assert parts[0] == 'NOT'
+		return 2**16-1 - eval(parts[1])
 	else:
-		left, op_name, right = instruction.split(' ')
-		op = binops = {
+		left, op_name, right = parts
+		return {
 			'RSHIFT': lambda a, b: a >> b,
 			'LSHIFT': lambda a, b: (a << b) % 2**16,
 			'AND': lambda a, b: a & b,
 			'OR': lambda a, b: a | b,
-		}[op_name]
-		return op(eval(left), eval(right))
+		}[op_name](eval(left), eval(right))
 
 instructions = {}
-values = {}
 for line in open('7.txt'):
 	instruction, variable = line.strip().split(' -> ')
 	instructions[variable] = instruction
@@ -37,6 +34,6 @@ for line in open('7.txt'):
 a_value = eval('a')
 print('Value of "a":', a_value)
 
-values.clear()
+signals.clear()
 instructions['b'] = str(a_value)
 print('New value of "a":', eval('a'))
