61074684d9a86b4593dd6eeb91a14b037601b506
BoppreH <boppreh@gmail.com>
2015-12-16 10:17:41 -0200

    Add correct solution to day 15

--- 15.py
+++ 15.py
@@ -1,24 +1,38 @@
 import re
-from itertools import combinations_with_replacement
 
-def totalize(ingredients, amounts):
+def totalize(ingredients, amounts, required_calories=None):
 	if sum(amounts) != 100: return 0
+	calories = sum(a*c for a, (*_, c) in zip(amounts, ingredients))
+	if required_calories and calories != required_calories: return 0
+
 	total = 1
-	for props in zip(*ingredients):
+	for props in list(zip(*ingredients))[:-1]:
 		total *= max(0, sum(i*a for i, a in zip(props, amounts)))
 	return total
 
 def parse_ingredients(text):
-	return [[int(i) for i in re.findall('(-?\d+)', line)][:-1] for line in text.split('\n') if line]
+	return [[int(i) for i in re.findall('(-?\d+)', line)] for line in text.split('\n') if line]
 
-def best_combination(ingredients):
-	combinations = combinations_with_replacement(range(101), len(ingredients))
-	key = lambda c: totalize(ingredients, c)
-	return max(combinations, key=key)
+def combinations(n, total=100, start=[]):
+	if n == 1:
+		yield start + [total]
+		return
+	for i in range(total+1):
+		yield from combinations(n-1, total-i, start=start+[i])
 
-assert totalize(parse_ingredients("""
+def best_combination(ingredients, total=100, calories=None):
+	return max(combinations(len(ingredients), total),
+			   key=lambda c: totalize(ingredients, c, calories))
+
+test_ingredients = parse_ingredients("""
 Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
 Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3
-"""), [44, 56]) == 62842880
+""")
+assert best_combination(test_ingredients) == [44, 56]
+assert totalize(test_ingredients, [44, 56]) == 62842880
 
-print('Best possible score:', best_combination(parse_ingredients(open('15.txt').read())))
+
+ingredients = parse_ingredients(open('15.txt').read())
+print('Best possible score:', totalize(ingredients, best_combination(ingredients)))
+
+print('Best possible score with 500 calories:', totalize(ingredients, best_combination(ingredients, calories=500)))
