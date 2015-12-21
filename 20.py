1c090b6a51b51a6e2159f9cd08bb755abf2ec669
BoppreH <boppreh@gmail.com>
2015-12-20 23:07:29 -0200

    Slight improvements to day 20

--- 20.py
+++ 20.py
@@ -1,24 +1,21 @@
-def visit(houses, elf_range, presents):
+def visit(houses, presents, elf_range):
 	for elf in range(1, len(houses)):
 		for i in elf_range(elf):
-			if i >= len(houses): break
 			houses[i] += presents * elf
 
 def first_house(houses, goal):
-	for i, presents in enumerate(houses):
-		if presents >= goal:
-			return i
+	return next(i for i, presents in enumerate(houses) if presents >= goal)
 
 
 N = int(1e6)
 
 houses = [0] * N
-visit(houses, lambda elf: range(elf, N, elf), 10)
+visit(houses, 10, lambda elf: range(elf, N, elf))
 
 assert houses[1:10] == [10, 30, 40, 70, 60, 120, 80, 150, 130]
 
 print('First house with at least 36000000 presents:', first_house(houses, 36000000))
 
 houses = [0] * N
-visit(houses, lambda elf: range(elf, 51 * elf, elf), 11)
+visit(houses, 11, lambda elf: range(elf, min(N, 51 * elf), elf))
 print('First house with at least 36000000 presents (lazy elves):', first_house(houses, 36000000))
